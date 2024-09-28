850c591b608e43693cb2a57e5e3cad0abe285a91
fengwanli <daillow@gmail.com>
2024-09-28 22:53:27 +0800

    抽奖文件，改英文名

--- notes_web/start.sh
+++ notes_web/start.sh
@@ -6,18 +6,14 @@ server="notes"
 export PYTHONDONTWRITEBYTECODE=x  # 不生成 pyc 文件
 export PORT=8080  # 设置端口号
 
-
 filename="$server".py
 if [ ! -f "$filename" ]; then filename="$filename"c;fi
-
 
 echo "清理旧程序"
 ps aux | grep "$server" | grep -v -w grep | awk '{print $2}' | xargs kill -9
 
-
 echo "启动 web 程序"
 nohup python3 "$filename" > /dev/null 2>&1 &
-
 
 echo "查看程序启动情况"
 ps aux | grep "$server"
